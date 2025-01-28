e3020f313f03d5e281a1e82cf36f7f6c6e207770
Kawish Hussain <kawish791@gmail.com>
2025-01-28 19:47:05 +0500

    chore: update project configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import ReactComponentName from "react-scan/react-component-name/webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true,
+  },
+  webpack: (config) => {
+    config.plugins.push(ReactComponentName({}));
+    return config;
+  },
 };
 
 export default nextConfig;
